613fa21ad022ab118fc40f4dcb0badf84aef0819
nguyenthinh28902 <nndthinh28902@gmail.com>
2026-07-15 18:09:43 +0700

    refactor(core): add custom exception hierarchy for business logic

---
+++ UserService.Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserService.Application.Common.Models;
+
+namespace UserService.Application.Common.Exceptions
+{
+    public class NotFoundException : BaseException 
+    { 
+        public NotFoundException(string msg, List<string>? errors = null) : base(msg, 404, errors) {
+        } 
+    }
+}
---
+++ UserService.Application/Common/Models/BaseException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserService.Application.Common.Models
+{
+    public abstract class BaseException : Exception
+    {
+        public int StatusCode { get; }
+        public List<string> Errors { get; } = new();
+        protected BaseException(string message, int statusCode, List<string>? errors)
+            : base(message) { StatusCode = statusCode; Errors = errors ?? new(); }
+    }
+}
